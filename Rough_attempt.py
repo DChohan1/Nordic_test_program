79095e85ca36ea53550e0c07874819a2d0a6be6b
DChohan1 <daksh.chohan1@gmail.com>
2026-06-10 15:37:08 +0100

    Update Rough_attempt.py

--- Rough_attempt.py
+++ Rough_attempt.py
@@ -6,9 +6,8 @@ from dotenv import load_dotenv
 from google import genai
 
 
-load_dotenv()
+api_key_gemini = st.secrets["GEMINI_API_KEY"]
 
-api_key_gemini = os.getenv("GEMINI_API_KEY")
 
 client = genai.Client(api_key=api_key_gemini)
 
